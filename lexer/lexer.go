6f8b2d40832b8bc244ff55e4925aedc197cb2c14
Jon Staten <jon.staten@gmail.com>
2017-10-21 00:27:15 -0400

    fixed import refs

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,6 +1,6 @@
 package lexer
 
-import "monkey-lang/token"
+import "monkey/token"
 
 type Lexer struct {
 	input        string
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,7 +1,7 @@
 package lexer
 
 import (
-	"monkey-lang/token"
+	"monkey/token"
 	"testing"
 )
 
